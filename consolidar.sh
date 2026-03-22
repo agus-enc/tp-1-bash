7512e49a3676ee099d8b2dab60d9c945a5200b83
Agus <aencalada@fi.uba.ar>
2026-03-22 18:48:47 -0300

    - Añadi shopt para prevenir errores con el ciclo for - Hice que el script se ejecute en loop hasta que su proceso padre muera

--- consolidar.sh
+++ consolidar.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-# Usando un ciclo for recorre todos los archivos .txt de la carpeta entrada, 
-# les copia el contenido a $FILENAME.txt y despues los mueve a procesado.
-# Usa $(pwd) porque se corre desde menu.sh que esta en otro directorio, entonces necesita poner el path completo en vez del relativo
-for file in $(pwd)/EPNro1/entrada/*.txt; do
-    cat $file >> $(pwd)/EPNro1/salida/"$FILENAME.txt" &&
-    mv $file $(pwd)/EPNro1/procesado/
+# shopt permite cambiar el comportamiento predeterminado de la shell. En este caso, setea la opcion
+# nullglob, que hace que el ciclo for que viene despues se termine solo si ve que no hay archivos .txt.
+# Si no estuviera, cuando el for buscara por *.txt y no encontrase, pasaria *.txt como el nombre del archivo y haría crashear al cat.
+shopt -s nullglob
+
+# Este ciclo while hace que el script busque archivos .txt hasta que su proceso padre, menu.sh, se termine.
+# También redirige los errores a /dev/null para que no aparezcan en la pantalla cuando se termine el ciclo.
+while kill -0 $PPID 2>/dev/null; do 
+    # Usando un ciclo for recorre todos los archivos .txt de la carpeta entrada, les copia el contenido a $FILENAME.txt y despues los mueve a procesado.
+    # Usa $(pwd) porque se corre desde menu.sh que esta en otro directorio, entonces necesita poner el path completo en vez del relativo
+    for file in "$(pwd)"/EPNro1/entrada/*.txt; do
+        cat "$file" >> "$(pwd)"/EPNro1/salida/"$FILENAME.txt" &&
+        mv "$file" "$(pwd)"/EPNro1/procesado/
+    done
+    # Pausa el script por 3 segundos para no consumir recursos constantemente.
+    sleep 3
 done
