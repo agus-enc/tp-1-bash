126460e21b5d1163abbd0fea41856eff1852cf71
Agus <aencalada@fi.uba.ar>
2026-03-24 19:47:12 -0300

    -

---
+++ consolidar.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# shopt permite cambiar el comportamiento predeterminado de la shell. En este caso, setea la opcion
+# nullglob, que hace que el ciclo for que viene despues se termine solo si ve que no hay archivos .txt.
+# Si no estuviera, cuando el for buscara por *.txt y no encontrase, pasaria *.txt como el nombre del archivo y haría crashear al cat.
+shopt -s nullglob
+
+# Usando un ciclo for recorre todos los archivos .txt de la carpeta entrada, les copia el contenido a $FILENAME.txt y despues los mueve a procesado.
+# Usa $(pwd) porque se corre desde menu.sh que esta en otro directorio, entonces necesita el path completo en vez del relativo.
+while true; do 
+    for file in "$(pwd)"/EPNro1/entrada/*.txt; do
+        cat "$file" >> "$(pwd)"/EPNro1/salida/"$FILENAME.txt" &&
+        mv "$file" "$(pwd)"/EPNro1/procesado/
+    done
+    # Pausa el proceso por 3 segundos para no saturar la CPU
+    sleep 3
+done
